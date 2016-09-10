488f15026308b5f59752b19a45f12ec85b53c90c
jndewey <jndewey@gmail.com>
2016-09-10 15:12:12 +0100

    added contract functions

--- contracts/GovContracts.sol
+++ contracts/GovContracts.sol
@@ -4,6 +4,7 @@ contract GovContracts {
 		uint128		id;
 		bytes		termsHash;
 		bool		projectDone;
+		address 	government;
 	}
 
 	struct Milestone {
@@ -11,6 +12,64 @@ contract GovContracts {
 		uint128 	contractId;
 		uint128 	duration;
 		uint128		targetBudget;
+		bool		withdrawn;
 	}
+
+}
+
+contract ContractUtility {
+
+    function buildContract(uint128 id, bytes contractHash) { 
+    	// Create a new government contract 
+
+    	// BlockOne Modifer needed (only Government)
+    	
+    	var NewContract GovContract;
+    	NewContract.id = id;
+    	NewContract.termsHash = contractHash;
+    	NewContract.government = msg.sender;
+    	NewContract.projectDone = false;
+    }
+ 
+   mapping(uint128 => GovContract) contract;
+
+   uint32 public numberContracts;
+   numberContracts = 0;
+
+    function terminateContract(uint128 id) { 
+    	// Sets completion attribute to true in order to flush
+    	// Modifier needed (only Government that submitted)
+
+    	Contract.projectDone = true;
+    }
+    
+    function registerContract(GovContract subContract) {
+    	subContract
+    }
+}
+contract MilestoneUtility {
+
+	function buildMilestone() {
+		var NewMilestone Milestone;
+    	Milestone.id = id;
+    	Milestone.contractId = ;
+    	NewContract.duration = ;
+    	NewContract.targetBudget = ;
+    	NewContract.withdrawn = false;
+    }
+
+    function withdrawMilestone() { // Modifier needed (only Government that submitted)
+        New = true;
+    }
+    
+    function registerContract(GovContract subContract) {
+    	
+	}
+}
+
+
+
+}
+
 
 }
