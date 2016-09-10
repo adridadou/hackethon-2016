120db14d51c51db9efda1f1a2385208e544fb590
adridadou <david@roon.me>
2016-09-10 13:33:45 +0100

    fix contract

--- contracts/GovContract.sol
+++ contracts/GovContract.sol
@@ -4,13 +4,13 @@ contract GovContracts {
 		uint128		id;
 		bytes		termsHash;
 		bool		projectDone
-	}
+	};
 
 	struct Milestone {
 		uint128 	id;
 		uint128 	contractId;
 		uint128 	duration;
 		uint128		targetBudget
-	}
+	};
 
 }
