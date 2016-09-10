eeb9a7f8c312c1668d5337ca11cc6b903f41f7ec
jndewey <jndewey@gmail.com>
2016-09-10 15:33:24 +0100

    clean up

--- contracts/GovContracts.sol
+++ contracts/GovContracts.sol
@@ -6,6 +6,51 @@ contract GovContracts {
 		bool		projectDone;
 		address 	government;
 	}
+	// Create type structure for government contracts that governments can 
+	// build and register with our smart contract on the blockchain 
+
+	mapping(uint128 => GovContract) contracts;
+	// map unique id numbers for all government contracts
+
+	uint32 public numberContracts;
+	numberContracts = 0;
+	// create counter for the number of government contracts and initially set to 0
+
+    function buildContract(uint128 id, bytes contractHash) { 
+    	// Create a new government contract 
+    	// BlockOne Modifer needed (only Government)
+    	
+    	var NewContract GovContract;
+    	NewContract.id = id;
+    	NewContract.termsHash = contractHash;
+    	NewContract.government = msg.sender;
+    	NewContract.projectDone = false;
+
+    }
+
+    function terminateContract(uint128 id) { 
+    	// Sets completion attribute to true in order to flush
+    	// Modifier needed (only Government that submitted)
+    	contracts[id].projectDone = true;
+    }
+    
+    function registerContract(GovContract subContract) {
+    	subContract
+    }
+
+    function getNumberContracts() {
+
+    }
+
+    function getContractHash(uint128 id) {
+    	var returnHash;
+    	returnHash = contracts[id].termsHash;
+    	return returnHash;
+    }
+}
+
+contract MilestoneUtility {
+
 
 	struct Milestone {
 		uint128 	id;
@@ -14,40 +59,6 @@ contract GovContracts {
 		uint128		targetBudget;
 		bool		withdrawn;
 	}
-
-}
-
-contract ContractUtility {
-
-    function buildContract(uint128 id, bytes contractHash) { 
-    	// Create a new government contract 
-
-    	// BlockOne Modifer needed (only Government)
-    	
-    	var NewContract GovContract;
-    	NewContract.id = id;
-    	NewContract.termsHash = contractHash;
-    	NewContract.government = msg.sender;
-    	NewContract.projectDone = false;
-    }
- 
-   mapping(uint128 => GovContract) contract;
-
-   uint32 public numberContracts;
-   numberContracts = 0;
-
-    function terminateContract(uint128 id) { 
-    	// Sets completion attribute to true in order to flush
-    	// Modifier needed (only Government that submitted)
-
-    	Contract.projectDone = true;
-    }
-    
-    function registerContract(GovContract subContract) {
-    	subContract
-    }
-}
-contract MilestoneUtility {
 
 	function buildMilestone() {
 		var NewMilestone Milestone;
